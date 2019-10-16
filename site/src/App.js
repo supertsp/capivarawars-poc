4cea69fb89fdab6f8a3b8f6dced5563813b2e2a3
Josh7GAS <joshuaandre@gmail.com>
2019-10-16 18:12:09 -0300

    pequenos ajustes

--- site/src/App.js
+++ site/src/App.js
@@ -4,7 +4,7 @@ import React, { Component } from 'react';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
-injectTapEventPlugin();
+// injectTapEventPlugin();
 
 class App extends Component {
   constructor(props){
