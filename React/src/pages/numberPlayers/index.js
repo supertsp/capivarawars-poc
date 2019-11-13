e29a7b6dbbc4060881f97082adc6245e169906c8
ImOhashi <leonardoohashi.mkt@gmail.com>
2019-11-13 20:58:48 -0300

    Tela de numero de jogadores

--- React/src/pages/numberPlayers/index.js
+++ React/src/pages/numberPlayers/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMinus, faPlus } from "@fortawesome/free-solid-svg-icons";
 
 import "./styles.css";
 
@@ -7,36 +9,65 @@ import imagemMar from "../../assets/images/imagem-mar.jpg";
 import capivaraLogo from "../../assets/images/CapivaraWars-logo.png";
 
 class NumberPlayers extends Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
+    this.state = {
+      numberPlayers: 2
+    };
 
-        }
-    }
+    this.valuePlus = () => {
+      this.setState({numberPlayers: this.state.numberPlayers + 1});
+    };
 
-    render() {
-        return (
-            <div>
-                <img src={imagemMar} className="background"></img>
-                <div className="container">
-                    <div className=" logo-cap">
-                        <img src={capivaraLogo} className="img-fluid img-thumbnail"></img>
-                    </div>
-                    <div className="card-num">
-                        <div className="card-body">
-                            <ul>
-                                <li><button type="button" class="btn btn-primary">Adicionar</button></li>
-                                <li>Número</li>
-                                <li><button type="button" class="btn btn-primary">Retirar</button></li>
-                                <button type="button" class="btn btn-primary botao-continuar">Continuar</button>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
+    this.valueMin = () => {
+      if (this.state.numberPlayers > 2) {
+        this.setState({numberPlayers: this.state.numberPlayers - 1});
+      }
+    };
+  }
+
+  render() {
+
+    const { numberPlayers } = this.state
+
+    return (
+      <div>
+        <img src={imagemMar} className="background"></img>
+        <div className="container">
+          <div className="container">
+            <div className="row">
+              <img
+                src={capivaraLogo}
+                className="img-fluid img-fluid-num img-thumbnail img-thumbnail-num"
+              ></img>
             </div>
-        );
-    }
+            <div className="row">
+              <div className="col-md-12">
+                <div className="card">
+                  <div className="card-body card-body-num">
+                    <div className="row">
+                      <FontAwesomeIcon
+                        icon={faMinus}
+                        onClick={this.valueMin}
+                        className="number"
+                      ></FontAwesomeIcon>
+                      <h1 className="number">{numberPlayers}</h1>
+                      <FontAwesomeIcon
+                        icon={faPlus}
+                        onClick={this.valuePlus}
+                        className="number"
+                      ></FontAwesomeIcon>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default NumberPlayers;
